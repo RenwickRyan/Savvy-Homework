22f82b0ee88102062a38156ce84346b24c387af9
Ryan Renwick <RRenwick@live.com>
2020-09-05 16:21:06 -0500

    add homework

---
+++ week2homework.js/pizzaplace.js
@@ -0,0 +1,21 @@
+let pizzaPlace = `Satan's Pizza Den`;
+let numberOfToppings = 666;
+
+console.log(pizzaPlace);
+console.log(numberOfToppings);
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+
+console.log(
+  `Welcome to the, ` +
+    pizzaPlace +
+    ` home of over ` +
+    numberOfToppings +
+    ` toppings!`
+);
+
+if (numberOfToppings < 10) {
+  console.log(`"Quality, not quantity."`);
+} else {
+  console.log(`"A whole lot of pizza."`);
+}
