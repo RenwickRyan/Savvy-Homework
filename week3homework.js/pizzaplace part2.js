bff0efc15cb609a8b76687b19dd434f13cae1fdb
Ryan Renwick <RRenwick@live.com>
2020-09-14 17:05:57 -0500

    add homework again

--- week3homework.js/pizzaplace part2.js
+++ week3homework.js/pizzaplace part2.js
@@ -3,28 +3,28 @@ let crust = ["thin crust", " thick crust"];
 let customerName = " Ryan";
 
 const pizzaToppings = ["pepperoni", "sausage", "bacon", "pineapple"];
+let customerOrder1 = [size[1], crust[1], pizzaToppings[1], pizzaToppings[2]];
 
-for (let Toppings of pizzaToppings) {
-  console.log(Toppings);
-}
 function greetCustomer() {
+  for (let Toppings of pizzaToppings) {
+    console.log(Toppings);
+  }
   return `Welcome to Devil's Pizza Den. Our toppings are ${pizzaToppings}`;
 }
-console.log(greetCustomer());
-
-let customerOrder1 = [size[1], crust[1], pizzaToppings[1], pizzaToppings[2]];
 
 function getPizzaOrder() {
   return `One ${customerOrder1} pizza for ${customerName}coming right up!`;
 }
-console.log(getPizzaOrder());
 
 function preparePizza() {
   return `Prepare...... ${customerOrder1} pizza for ${customerName}.`;
 }
-console.log(preparePizza());
 
 function servePizza() {
-  return `Order up! Here is your ${customerOrder1} pizza...Enjoy!`;
+  return `Order up! Here is your ${customerOrder1} pizza ......Enjoy!`;
 }
+
+console.log(greetCustomer());
+console.log(getPizzaOrder());
+console.log(preparePizza());
 console.log(servePizza());
