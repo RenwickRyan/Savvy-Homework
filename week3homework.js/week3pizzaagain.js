829ea26513b9c247e9bf000218c8947f173e745c
Ryan Renwick <RRenwick@live.com>
2020-09-16 17:36:31 -0500

    add most up to date homework

--- week3homework.js/week3pizzaagain.js
+++ week3homework.js/week3pizzaagain.js
@@ -1,4 +1,6 @@
 const pizzaToppings = ["pepperoni", "sausage", "bacon", "pineapple"];
+const crust = ["thin crust", "thick crust"];
+const size = ["small", "medium", "large", "extra large"];
 
 function greetCustomer() {
   for (let Toppings of pizzaToppings) {
@@ -13,16 +15,22 @@ function getPizzaOrder(size, crust, ...pizzaToppings) {
   console.log(`One ${size}${crust}${pizzaToppings} pizza for coming right up!`);
   return size + crust + pizzaToppings;
 }
-getPizzaOrder("small", "thin crust", "pepperoni", "sausage", "pineapple");
+let order1 = getPizzaOrder(
+  size[1],
+  crust[1],
+  pizzaToppings[0],
+  pizzaToppings[1],
+  pizzaToppings[2]
+);
 
-function preparePizza(...getPizzaOrder) {
-  console.log(`coming up......${getPizzaOrder} pizza.`);
+function preparePizza() {
+  console.log(`...cooking pizza...`);
   return getPizzaOrder;
 }
-preparePizza("small", "thin crust", "pepperoni", "sausage", "pineapple");
+preparePizza(order1);
 
-function servePizza(...getPizzaOrder) {
-  console.log(`Order up! Here is your ${getPizzaOrder} pizza...Enjoy!`);
+function servePizza() {
+  console.log(`Order up! Here is your ${order1} pizza...Enjoy!`);
   return getPizzaOrder;
 }
-servePizza("small", "thin crust", "pepperoni", "sausage", "pineapple");
+servePizza(order1);
