4522320b034d0707089d7d2588243a7aa37bb676
Ryan Renwick <RRenwick@live.com>
2020-09-16 14:50:01 -0500

    week3 homework

--- week3homework.js/pizzaplace part2.js
+++ week3homework.js/pizzaplace part2.js
@@ -1,5 +1,6 @@
-const size = ["small", "medium", "large"];
-const crust = ["thin crust", " thick crust"];
+let size = ["small", "medium", "large"];
+let crust = ["thin crust", " thick crust"];
+
 let customerName = " Ryan";
 
 const pizzaToppings = ["pepperoni", "sausage", "bacon", "pineapple"];
@@ -12,19 +13,15 @@ function greetCustomer() {
   }
   return `Welcome to Devil's Pizza Den. Our toppings are ${pizzaToppings}`;
 }
-
 function getPizzaOrder() {
   return `One ${pizzaObject} pizza for ${customerName}coming right up!`;
 }
-
 function preparePizza() {
   return `Prepare...... ${pizzaObject} pizza for ${customerName}.`;
 }
-
 function servePizza() {
   return `Order up! Here is your ${pizzaObject} pizza...Enjoy!`;
 }
-
 console.log(greetCustomer());
 console.log(getPizzaOrder());
 console.log(preparePizza());
