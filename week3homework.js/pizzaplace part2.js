5a10bc06c80f4ac5388cca9f09b68a1fbca2f012
Ryan Renwick <RRenwick@live.com>
2020-09-16 17:37:14 -0500

    add same as week3pizzaagain now

--- week3homework.js/pizzaplace part2.js
+++ week3homework.js/pizzaplace part2.js
@@ -1,46 +1,36 @@
-let size = ["small", "medium", "large"];
-let crust = ["thin crust", "thick crust"];
-
-let customerName = "Ryan";
-
 const pizzaToppings = ["pepperoni", "sausage", "bacon", "pineapple"];
-
-let pizzaObject = [size[1], crust[1], ...pizzaToppings];
+const crust = ["thin crust", "thick crust"];
+const size = ["small", "medium", "large", "extra large"];
 
 function greetCustomer() {
   for (let Toppings of pizzaToppings) {
-    console.log(Toppings);
   }
-  return `Welcome to Devil's Pizza Den. Our toppings are ${pizzaToppings}`;
+  console.log(
+    `Welcome to Devil's Pizza Den. Our toppings are ${pizzaToppings}`
+  );
 }
-function getPizzaOrder() {
-  return `One ${pizzaObject} pizza for ${customerName}coming right up!`;
-}
-function preparePizza() {
-  return `Prepare...... ${pizzaObject} pizza for ${customerName}.`;
-}
-function servePizza() {
-  return `Order up! Here is your ${pizzaObject} pizza...Enjoy!`;
-}
-console.log(greetCustomer());
-console.log(getPizzaOrder());
-console.log(preparePizza());
-console.log(servePizza());
+greetCustomer();
 
-// below is more of me tryign to figure out passing in things to the function
-
-function getPizzaOrder2(name, size, crust, ...pizzaToppings) {
-  console.log("customer name: " + name);
-  console.log(size);
-  console.log(crust);
-  console.log(pizzaToppings);
-  return name + size + crust + pizzaToppings;
+function getPizzaOrder(size, crust, ...pizzaToppings) {
+  console.log(`One ${size}${crust}${pizzaToppings} pizza for coming right up!`);
+  return size + crust + pizzaToppings;
 }
-getPizzaOrder2(
-  "ryan",
-  "small",
-  "thin crust",
-  "pepperoni",
-  "sausage",
-  "pineapple"
+let order1 = getPizzaOrder(
+  size[1],
+  crust[1],
+  pizzaToppings[0],
+  pizzaToppings[1],
+  pizzaToppings[2]
 );
+
+function preparePizza() {
+  console.log(`...cooking pizza...`);
+  return getPizzaOrder;
+}
+preparePizza(order1);
+
+function servePizza() {
+  console.log(`Order up! Here is your ${order1} pizza...Enjoy!`);
+  return getPizzaOrder;
+}
+servePizza(order1);
